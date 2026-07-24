7830c7135d8c14a4dccd7103d3f740de9daaf1fb
Venus Pundir <venupundir05@gmail.com>
2026-07-24 17:09:56 +0530

    commit chnges

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  base: '/Quote-of-the-day/', // Set the base path for the application
 })
